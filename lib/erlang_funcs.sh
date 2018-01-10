bb3155d57874fcd71a64dad146a9852fd4e7d210
João Britto <jabcalves@gmail.com>
2018-01-10 20:47:25 -0200

    More debugging

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -29,27 +29,42 @@ function clean_erlang_downloads() {
 function install_erlang() {
   output_section "Installing Erlang ${erlang_version} $(erlang_changed)"
 
-  output_section "Build path: ${build_path}"
-  output_section "Runtime path: ${runtime_path}"
-  output_section "Cache path: ${cache_path}"
+  output_line "Build path: ${build_path}"
+  output_line "Runtime path: ${runtime_path}"
+  output_line "Cache path: ${cache_path}"
 
-  output_section "Erlang path: $(erlang_path)"
-  output_section "Erlang build path: $(erlang_build_path)"
-  output_section "Runtime Erlang path: $(runtime_erlang_path)"
-  output_section "Runtime platform tools path: $(runtime_platform_tools_path)"
-  output_section "Platform tools path: $(platform_tools_path)"
-  output_section "Elixir path: $(elixir_path)"
+  output_line "Platform tools path: $(platform_tools_path)"
+  output_line "Erlang path: $(erlang_path)"
 
-  rm -rf $(erlang_build_path)
-  mkdir -p $(erlang_build_path)
+  output_line "Runtime platform tools path: $(runtime_platform_tools_path)"
+  output_line "Runtime Erlang path: $(runtime_erlang_path)"
+
+  output_line "Elixir path: $(elixir_path)"
+  output_line "Erlang build path: $(erlang_build_path)"
+
+  output_line "rm -rf $(erlang_build_path)"
+  rm -vrf $(erlang_build_path)
+  output_line "mkdir -vp $(erlang_build_path)"
+  mkdir -vp $(erlang_build_path)
+  output_line "tar zxf ${cache_path}/$(erlang_tarball) -C $(erlang_build_path) --strip-components=1"
   tar zxf ${cache_path}/$(erlang_tarball) -C $(erlang_build_path) --strip-components=1
 
-  rm -rf $(runtime_erlang_path)
-  mkdir -p $(runtime_platform_tools_path)
-  ln -s $(erlang_build_path) $(runtime_erlang_path)
+  output_line "rm -rf $(runtime_erlang_path)"
+  rm -vrf $(runtime_erlang_path)
+  output_line "mkdir -p $(runtime_platform_tools_path)"
+  mkdir -vp $(runtime_platform_tools_path)
+  output_line "ln -s $(erlang_build_path) $(runtime_erlang_path)"
+  ln -vs $(erlang_build_path) $(runtime_erlang_path)
+  output_line "$(erlang_build_path)/Install -minimal $(runtime_erlang_path)"
   $(erlang_build_path)/Install -minimal $(runtime_erlang_path)
 
-  cp -R $(erlang_build_path) $(erlang_path)
+  output_line "cp -R $(erlang_build_path) $(erlang_path)"
+  cp -vR $(erlang_build_path) $(erlang_path)
+
+  ls -la $(erlang_build_path)
+  ls -la $(erlang_path)
+
+  output_line "setting PATH to: $(erlang_path)/bin:$PATH"
   PATH=$(erlang_path)/bin:$PATH
 }
 
